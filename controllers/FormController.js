17d40fbce732b96cca19d7a9be43d67add6028c3
ariefrzld <ariefrizaldy5@gmail.com>
2024-02-03 10:44:43 +0700

    module form: delete, index by userId, pagination

--- controllers/FormController.js
+++ controllers/FormController.js
@@ -2,6 +2,22 @@ import mongoose from "mongoose";
 import Form from "../models/Form.js";
 
 class FormController {
+  async index(req, res) {
+    try {
+      const limit = parseInt(req.query.limit) || 10;
+      const page = parseInt(req.query.page) || 1;
+      const form = await Form.paginate({ userId: req.jwt.id }, { limit: limit, page: page });
+      if (!form) {
+        throw { code: 404, message: "FORMS_NOT_FOUND" };
+      }
+      return res.status(200).json({ status: true, message: "FORMS_FOUND", total: form.length, form });
+    } catch (error) {
+      return res.status(error.code || 500).json({
+        status: false,
+        message: error.message,
+      });
+    }
+  }
   async store(req, res) {
     try {
       const form = await Form.create({
@@ -57,10 +73,31 @@ class FormController {
       }
       const form = await Form.findOneAndUpdate({ _id: req.params.id, userId: req.jwt.id }, req.body, { new: true });
       if (!form) {
-        throw { code: 404, message: "FORM_UPDATE_NOT_FAILED" };
+        throw { code: 404, message: "FORM_UPDATE_FAILED" };
       }
 
       return res.status(200).json({ status: true, message: "FORM_UPDATE_SUCCESS", form });
+    } catch (error) {
+      return res.status(error.code || 500).json({
+        status: false,
+        message: error.message,
+      });
+    }
+  }
+  async destroy(req, res) {
+    try {
+      if (!req.params.id) {
+        throw { code: 400, message: "REQUIRED_FORM_ID" };
+      }
+      if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
+        throw { code: 400, message: "INVALID_FORM_ID" };
+      }
+      const form = await Form.findOneAndDelete({ _id: req.params.id, userId: req.jwt.id });
+      if (!form) {
+        throw { code: 404, message: "FORM_DELETE_FAILED" };
+      }
+
+      return res.status(200).json({ status: true, message: "FORM_DELETE_SUCCESS", form });
     } catch (error) {
       return res.status(error.code || 500).json({
         status: false,
