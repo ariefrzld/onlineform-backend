a2caf58b2a79304a04d206f4dd4a72926b723336
ariefrzld <ariefrizaldy5@gmail.com>
2024-02-04 07:44:41 +0700

    module question: delete, list

--- controllers/QuestionController.js
+++ controllers/QuestionController.js
@@ -4,6 +4,28 @@ import Form from "../models/Form.js";
 const allowedTypes = ["Text", "Email", "Radio", "Checkbox", "Dropdown"];
 
 class QuestionController {
+  async index(req, res) {
+    try {
+      if (!req.params.id) {
+        throw { code: 400, message: "REQUIRED_FORM_ID" };
+      }
+      if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
+        throw { code: 400, message: "INVALID_ID" };
+      }
+      const form = await Form.findOne({ _id: req.params.id, userId: req.jwt.id });
+      if (!form) {
+        throw { code: 404, message: "FORM_NOT_FOUND" };
+      }
+
+      return res.status(200).json({ status: true, message: "FORM_FOUND", form });
+    } catch (error) {
+      return res.status(error.code || 500).json({
+        status: false,
+        message: error.message,
+      });
+    }
+  }
+
   async store(req, res) {
     try {
       if (!req.params.id) {
@@ -47,6 +69,9 @@ class QuestionController {
       if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
         throw { code: 428, message: "INVALID_ID" };
       }
+      if (!mongoose.Types.ObjectId.isValid(req.params.questionId)) {
+        throw { code: 428, message: "INVALID_ID" };
+      }
 
       let field = {};
       if (req.body.hasOwnProperty("question")) {
@@ -66,6 +91,37 @@ class QuestionController {
       }
 
       return res.status(200).json({ status: true, message: "UPDATE_QUESTION_SUCCESS", question });
+    } catch (error) {
+      return res.status(error.code || 500).json({
+        status: false,
+        message: error.message,
+      });
+    }
+  }
+
+  async destroy(req, res) {
+    try {
+      if (!req.params.id) {
+        throw { code: 428, message: "FORM_ID_REQUIRED" };
+      }
+      if (!req.params.id) {
+        throw { code: 428, message: "QUESTION_ID_REQUIRED" };
+      }
+      if (!mongoose.Types.ObjectId.isValid(req.params.id)) {
+        throw { code: 428, message: "INVALID_ID" };
+      }
+      if (!mongoose.Types.ObjectId.isValid(req.params.questionId)) {
+        throw { code: 428, message: "INVALID_ID" };
+      }
+
+      //update form
+      const form = await Form.findOneAndUpdate({ _id: req.params.id, userId: req.jwt.id }, { $pull: { questions: { id: new mongoose.Types.ObjectId(req.params.questionId) } } }, { new: true });
+
+      if (!form) {
+        throw { code: 400, message: "DELETE_QUESTION_FAILED" };
+      }
+
+      return res.status(200).json({ status: true, message: "DELETE_QUESTIONS_SUCCESS", form });
     } catch (error) {
       return res.status(error.code || 500).json({
         status: false,
