fcf918504573bd422e7937379952d03c482364c7
ariefrzld <ariefrizaldy5@gmail.com>
2024-02-04 22:33:05 +0700

    module option: store, update, delete

--- routes/api.js
+++ routes/api.js
@@ -3,6 +3,7 @@ import jwtAuth from "../middlewares/jwtAuth.js";
 import AuthController from "../controllers/AuthController.js";
 import FormController from "../controllers/FormController.js";
 import QuestionController from "../controllers/QuestionController.js";
+import OptionController from "../controllers/OptionController.js";
 
 const router = express.Router();
 
@@ -23,5 +24,10 @@ router.get("/forms/:id/questions", jwtAuth(), QuestionController.index);
 router.post("/forms/:id/questions", jwtAuth(), QuestionController.store);
 router.put("/forms/:id/questions/:questionId", jwtAuth(), QuestionController.update);
 router.delete("/forms/:id/questions/:questionId", jwtAuth(), QuestionController.destroy);
+
+// Options
+router.post("/forms/:id/questions/:questionId/options", jwtAuth(), OptionController.store);
+router.put("/forms/:id/questions/:questionId/options/:optionId", jwtAuth(), OptionController.update);
+router.delete("/forms/:id/questions/:questionId/options/:optionId", jwtAuth(), OptionController.destroy);
 
 export default router;
